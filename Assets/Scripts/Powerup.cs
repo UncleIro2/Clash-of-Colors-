741aa2e8db5a2cba3a4fc5c5d2268e5d9494da5c
neburenus <sunespam@posselt.dk>
2023-12-12 12:24:24 +0100

    Revert "Merge branch 'main' of https://github.com/UncleIro2/Clash-of-Colors-"
    This reverts commit d5335952acec045b10654b9e741a32b619e7fa48, reversing
    changes made to 2c83272b0980ad4a1d9449e4afd6578054f388ed.

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -6,95 +6,55 @@ public class Powerup : MonoBehaviour
     //coledown time 
     public float cooldownTime = 1.0f;
     private bool isCooldown = false;
-  
 
     private void Start()
     {
         //coledown time i starten når objektet ikke er der 
         StartCoroutine(EnableAndDisable(10f));
-       
-
     }
 
 
     void OnTriggerEnter2D(Collider2D other)
     {
-   
-            //Kører rotinen 
-            if (!isCooldown)
+        //Kører rotinen 
+        if (!isCooldown)
+        {
+            StartCoroutine(EnableAndDisable(cooldownTime));
+
+        }
+        if (other.gameObject.CompareTag("Player 1"))
+        {
+            // Lader dette script påvirke playercontroller når der bliver collided med GameObject
+            PlayerController PlayerController = other.gameObject.GetComponent<PlayerController>();
+
+            // Checker for komponenter i playercontroller
+            if (PlayerController != null)
             {
-                StartCoroutine(EnableAndDisable(cooldownTime));
+                // Kan ændre på movespeed nu
+                PlayerController.moveSpeed = 5;
 
-            }
-            if (other.gameObject.CompareTag("Player 1"))
-            {
-                // Lader dette script påvirke playercontroller når der bliver collided med GameObject
-                PlayerController PlayerController = other.gameObject.GetComponent<PlayerController>();
-
-                int randomIndex = Random.Range(1, 3);
-
-                if (randomIndex == 1)
-                {
-                    
-                    // Kan ændre på movespeed nu
-                    PlayerController.moveSpeed = 5;
-
-                    //Ændre på farven
-                    PlayerController.ChangeColor("#FAA0A0");
-                    
-                }
-
-                else if (randomIndex == 2)
-                {               
-                    
-                    // Lader dig dashe 
-                    PlayerController.dashController = true;
-                    
-                     //Ændre på farven
-                    PlayerController.ChangeColor("#702963");
-                    
-                }
+                //Ændre på farven
+                PlayerController.ChangeColor("#FAA0A0");
             }
 
-
-
+        }
         if (other.gameObject.CompareTag("Player 2"))
         {
             // Lader dette script påvirke playercontroller når der bliver collided med GameObject
             PlayerController PlayerController = other.gameObject.GetComponent<PlayerController>();
 
-            int randomIndex = Random.Range(1, 3);
-
-            if (randomIndex == 1)
+            // Checker for komponenter i playercontroller
+            if (PlayerController != null)
             {
-                
                 // Kan ændre på movespeed nu
                 PlayerController.moveSpeed = 5;
 
                 //Ændre på farven
                 PlayerController.ChangeColor("#DFFF00");
 
-                
-            }
-
-            else if (randomIndex == 2)
-            {
-               
-                // Lader dig dashe 
-                PlayerController.dashController = true;
-
-                //Ændre på farven
-                PlayerController.ChangeColor("#C4B454");
-                
             }
         }
-
-
-        
-           
-        
     }
-
 
     IEnumerator EnableAndDisable(float cooldown)
     {
